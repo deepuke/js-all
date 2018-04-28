92c688d7e6f0c5e049352b71e5d116f8a35a6f12
deepuke <dipuke@gmail.com>
2018-04-28 11:31:42 +0530

    Output Management -- print and index js files to index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-function test() {
-    console.log('this is webpack test function');
+import printMe from './print.js';
+
+function component() {
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    return element;
 }
 
-test();
+document.body.appendChild(component());
