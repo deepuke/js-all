591504c811791afc7bf44f15a324f52cef19eaa7
deepuke <dipuke@gmail.com>
2018-04-29 08:21:10 +0530

    Hot Module Replacement - webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
-    
+
     var btn = document.createElement('button');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
@@ -11,3 +11,12 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
