28dd923157f66a455c77e870c0dde7e51604b6e2
deepuke <dipuke@gmail.com>
2018-04-29 15:03:34 +0530

    Tree shaking -  removing unwanted code[dead code]

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import printMe from './print.js';
 import './styles.css';
-
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
-    var btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
     return element;
 }
 
@@ -16,11 +15,3 @@ let element = component();  // Store the element to re-render on print.js change
 document.body.appendChild(element);
 
 
-if (module.hot) {
-    module.hot.accept('./print.js', function () {
-        console.log('Accepting the updated printMe module!');
-        document.body.removeChild(element);
-        element = component(); // Re-render the "component" to update the click handler
-        document.body.appendChild(element);
-    })
-}
--- src/print.js
+++
@@ -1,3 +0,0 @@
-export default function printMe() {
-    console.log('Updated print.js...');
-}
